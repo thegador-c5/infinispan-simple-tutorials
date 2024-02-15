aef24510cc14ecadf5c05b4565835cc8b0b06342
Tom Hegadorn <thegador@redhat.com>
2024-02-15 12:03:32 -0500

    Checking in change for custom example

---
+++ infinispan-remote/cache/src/main/java/org/infinispan/tutorial/simple/remote/MyInfinispanRemoteCache.java
@@ -0,0 +1,31 @@
+package org.infinispan.tutorial.simple.remote;
+
+import org.infinispan.client.hotrod.RemoteCache;
+import org.infinispan.client.hotrod.RemoteCacheManager;
+import org.infinispan.tutorial.simple.connect.TutorialsConnectorHelper;
+    
+import static org.infinispan.tutorial.simple.connect.TutorialsConnectorHelper.TUTORIAL_CACHE_NAME;
+    
+/**
+ *
+ * Infinispan Server includes a default property realm that requires
+ * authentication. Create some credentials before you run this tutorial.
+ *
+ */
+public class MyInfinispanRemoteCache {
+    
+    public static void main(String[] args) {
+        // Connect to the server
+        RemoteCacheManager cacheManager = TutorialsConnectorHelper.connect();
+        // Obtain the remote cache
+        RemoteCache<String, String> cache = cacheManager.getCache(TUTORIAL_CACHE_NAME);
+        /// Store a value
+        cache.put("mykey", "myvalue");
+        // Retrieve the value and print it out
+        System.out.printf("key = %s\n", cache.get("key"));
+        // Stop the cache manager and release all resources
+        cacheManager.stop();
+    }
+    
+}
+
